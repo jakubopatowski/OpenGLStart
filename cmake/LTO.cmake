524a4efe0cce5a34d246be84917c82925bab23f9
Jakub Opatowski <jakub.opatowski@gmail.com>
2024-10-22 22:14:24 +0200

    LTO support

---
+++ cmake/LTO.cmake
@@ -0,0 +1,11 @@
+function(target_enable_lto TARGET)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT result OUTPUT output)
+
+    if(result)
+        message(STATUS "IPO/LTO is supported!")
+        set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ON)
+    else()
+        message(STATUS "IPO/LTO is NOT supported!")
+    endif()
+endfunction(target_enable_lto)
